b95cc15283a8235f9d75d398d80a46966d6ef49b
Philip Suttie <philip.suttie@gmail.com>
2023-10-11 13:40:54 +0200

    Added a function in data_tranformations.r that completes the transformation of stations_metadata to a data frame. Solved problem 2!

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -1,2 +1,25 @@
 
+# Load necessary libraries
+library(tibble)
+library(purrr)
+
+# Define the function to transform metadata to a data frame
+transform_metadata_to_df <- function(stations_metadata) {
+  # Flatten the list structure and extract the necessary information
+  metadata_df <- stations_metadata$trafficRegistrationPoints %>%
+    map_df(~ {
+      latestData <- .x$latestData
+      list(
+        id = .x$id,
+        name = .x$name,
+        latestData = as.POSIXct(latestData$volumeByHour, tz = "UTC"),
+        lat = .x$location$coordinates$latLon$lat,
+        lon = .x$location$coordinates$latLon$lon
+      )
+    })
+  
+  return(metadata_df)
+}
+
+
 
